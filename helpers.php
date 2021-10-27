6bb520b99d285a4fd6741d6cb706deec8902cb80
Jeremiasz Major <jrh.mjr@gmail.com>
2021-10-27 23:30:43 +0200

    enable some fixers from php-cs-fixer-custom-fixers

--- helpers.php
+++ helpers.php
@@ -5,10 +5,12 @@ namespace Major\CS;
 use PhpCsFixer\Config;
 use PhpCsFixer\ConfigInterface;
 use PhpCsFixer\Finder;
+use PhpCsFixerCustomFixers\Fixers as CustomFixers;
 
 function config(Finder $finder, array $rules = []): ConfigInterface
 {
-    return (new Config())->setRules(
-        array_merge(require __DIR__.'/rules.php', $rules)
-    )->setFinder($finder);
+    return (new Config('jrmajor/php-cs'))
+        ->registerCustomFixers(new CustomFixers())
+        ->setRules(array_merge(require __DIR__ . '/rules.php', $rules))
+        ->setFinder($finder);
 }
