e4c26d544cbc3e3ad5b1eafbbb474f62ba598c95
Jeremiasz Major <jrh.mjr@gmail.com>
2022-02-08 00:51:21 +0100

    fix config name

--- helpers.php
+++ helpers.php
@@ -9,7 +9,7 @@ use PhpCsFixerCustomFixers\Fixers as CustomFixers;
 
 function config(Finder $finder, array $rules = []): ConfigInterface
 {
-    return (new Config('jrmajor/php-cs'))
+    return (new Config('jrmajor/cs'))
         ->registerCustomFixers(new CustomFixers())
         ->setRules(array_merge(require __DIR__ . '/rules.php', $rules))
         ->setFinder($finder);
