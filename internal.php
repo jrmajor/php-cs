346bde7791fca78a7ce612946390acabeeeca3d0
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-24 02:22:58 +0100

    add more internal helpers

--- internal.php
+++ internal.php
@@ -3,6 +3,27 @@
 namespace Major\CS\Internal;
 
 /**
+ * Denotes cases that are static analysis tooling responsibility.
+ */
+const STATIC_ANALYSIS = false;
+
+/**
+ * These fixers may be useful when working with legacy codebases.
+ * Use MAJOR_CS_LEGACY_RULES environment variable to enable them.
+ *
+ * @template T of true|array<string, mixed>
+ *
+ * @param T $options
+ * @return T|false
+ */
+function legacy(bool|array $options = true): bool|array
+{
+    $legacy = getenv('MAJOR_CS_LEGACY_RULES');
+
+    return $legacy === '1' ? $options : false;
+}
+
+/**
  * Enable or disable fixers depending on the current PHP version.
  *
  * @template Ta of true|array<string, mixed>
@@ -23,4 +44,17 @@ function version(
     $version = 10000 * $major + 100 * $minor;
 
     return PHP_VERSION_ID >= $version ? $after : $before;
+}
+
+/**
+ * Denotes rare cases, skipping them to speed up linting process.
+ *
+ * @template T of true|array<string, mixed>
+ *
+ * @param T $options
+ * @return T|false
+ */
+function rare(bool|array $options = true): bool|array
+{
+    return false;
 }
