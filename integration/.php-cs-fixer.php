539bb7661b0d6411822d26889b1db743b07d7e17
Jeremiasz Major <jrh.mjr@gmail.com>
2022-09-08 16:23:32 +0200

    convert integration checks to a workflow

--- integration/.php-cs-fixer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-    ->append(['check'])
-    ->ignoreVCSIgnored(true);
-
-return Major\CS\config($finder)
-    ->setCacheFile('../.cache/.php-cs-fixer.i.cache');
